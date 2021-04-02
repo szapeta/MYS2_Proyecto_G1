4157be731a1cea56481dc188ad1abb1ffaeac584
Jose Aguilar Sandoval <jose.aguilarportillo@conduent.com>
2021-04-01 20:21:18 -0600

    Update AnalisisProb.r
    cambiando la forma de averiguar las distribuciones de probabilidad para que no ejecute todas una vez haya sido aprobada por el programa

--- AnalisisProb.r
+++ AnalisisProb.r
@@ -4,12 +4,12 @@ library(fitdistrplus)
 library(rriskDistributions)
 library(readxl)
 library(XLConnect)
-
+#setwd("C:/Users/30201507/Desktop")
 
 buscarProb <- function(excelPath){
-
+  
   i = 1
-  notas <- loadWorkbook("[MYS2]OutputData_G1.xlsx")
+  notas <- loadWorkbook(excelPath)
   
   while (i<192) {
     
@@ -19,106 +19,72 @@ buscarProb <- function(excelPath){
     
     out <- tryCatch(
       {
-        print(i)
-        print("unif")
         ajuste <- fitdist(as.vector(t(lecturaCol)), "unif")
-        ajuste$estimate
         prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("norm")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "norm")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        #print("t")
-        #ajuste <- fitdist(as.vector(t(lecturaCol)), "t", start = list(df2))
-        #ajuste$estimate
-        #prueba <- gofstat(ajuste)
-        #prueba$kstest
-        #print(prueba$kstest)
-        #prueba$chisqpvalue
-        
-        #ajuste <- fitdist(as.vector(t(lecturaCol)), "chisq")
-        #ajuste$estimate
-        #prueba <- gofstat(ajuste)
-        #prueba$kstest
-        #print(prueba$kstest)
-        #prueba$chisqpvalue
-        
-        #if(grepl("not rejected", prueba$kstest)){
-        #  print("True")
-        #}else{
-        #  print("False")
-        #}
-        
-        print("weibull")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "weibull")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("pois")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "pois")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("geom")
-        juste <- fitdist(as.vector(t(lecturaCol)), "geom")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("nbinom")
-        juste <- fitdist(as.vector(t(lecturaCol)), "nbinom")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("exp")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "exp")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("gamma")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "gamma")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
+        if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+          print(i)
+          print("unif")
+        }else{
+          ajuste <- fitdist(as.vector(t(lecturaCol)), "norm")
+          prueba <- gofstat(ajuste)
+          if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+            print(i)
+            print("norm")
+          }else{
+            ajuste <- fitdist(as.vector(t(lecturaCol)), "weibull")
+            prueba <- gofstat(ajuste)
+            if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+              print(i)
+              print("weibull")
+            }else{
+              ajuste <- fitdist(as.vector(t(lecturaCol)), "gamma")
+              prueba <- gofstat(ajuste)
+              if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+                print(i)
+                print("gamma")
+              }else{
+                ajuste <- fitdist(as.vector(t(lecturaCol)), "geom")
+                prueba <- gofstat(ajuste)
+                if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+                  print(i)
+                  print("geom")
+                }else{
+                  ajuste <- fitdist(as.vector(t(lecturaCol)), "nbinom")
+                  prueba <- gofstat(ajuste)
+                  if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+                    print(i)
+                    print("nbinom")
+                  }else{
+                    ajuste <- fitdist(as.vector(t(lecturaCol)), "exp")
+                    prueba <- gofstat(ajuste)
+                    if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+                      print(i)
+                      print("exp")
+                    }else{
+                      ajuste <- fitdist(as.vector(t(lecturaCol)), "gamma")
+                      prueba <- gofstat(ajuste)
+                      if(length(prueba$kstest) & grepl("not rejected", prueba$kstest)){
+                        print(i)
+                        print("gamma")
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       },
       error=function(cond) {
         
         return(NA)
       }
       
-     
+      
     )    
-     i = i + 1
+    i = i + 1
   }
   
 }
 
 buscarProb("[MYS2]OutputData_G1.xlsx")
-
-
-
