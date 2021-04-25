2607f09f960f4b4f30202ab0b280fe9940e11c98
szapeta <2375894860101@ingenieria.usac.edu.gt>
2021-04-25 12:55:35 -0600

    Se

--- AnalisisProb.r
+++ AnalisisProb.r
@@ -2,119 +2,63 @@ library(MASS)
 library(survival)
 library(fitdistrplus)
 library(rriskDistributions)
-library(readxl)
 library(XLConnect)
 
+setwd("~/USAC/Modela 2/LAB/proyecto/MYS2_Proyecto_G1")
 
 buscarProb <- function(excelPath){
 
+  numCorrectas = 0
   i = 1
   notas <- loadWorkbook("[MYS2]OutputData_G1.xlsx")
   
-  while (i<192) {
-    
+  while (i<192) 
+  {
+    print(paste0("Columna: #",i))
     lecturaCol = readWorksheet(notas, sheet = 'Processing Data', startCol = i, endCol = i)
-    
-    
-    
-    out <- tryCatch(
-      {
-        print(i)
-        print("unif")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "unif")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("norm")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "norm")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        #print("t")
-        #ajuste <- fitdist(as.vector(t(lecturaCol)), "t", start = list(df2))
-        #ajuste$estimate
-        #prueba <- gofstat(ajuste)
-        #prueba$kstest
-        #print(prueba$kstest)
-        #prueba$chisqpvalue
-        
-        #ajuste <- fitdist(as.vector(t(lecturaCol)), "chisq")
-        #ajuste$estimate
-        #prueba <- gofstat(ajuste)
-        #prueba$kstest
-        #print(prueba$kstest)
-        #prueba$chisqpvalue
-        
-        #if(grepl("not rejected", prueba$kstest)){
-        #  print("True")
-        #}else{
-        #  print("False")
-        #}
-        
-        print("weibull")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "weibull")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("pois")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "pois")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("geom")
-        juste <- fitdist(as.vector(t(lecturaCol)), "geom")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("nbinom")
-        juste <- fitdist(as.vector(t(lecturaCol)), "nbinom")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("exp")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "exp")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-        print("gamma")
-        ajuste <- fitdist(as.vector(t(lecturaCol)), "gamma")
-        ajuste$estimate
-        prueba <- gofstat(ajuste)
-        prueba$kstest
-        print(prueba$kstest)
-        prueba$chisqpvalue
-        
-      },
-      error=function(cond) {
-        
-        return(NA)
-      }
+    vectorDatos = as.vector(t(lecturaCol))
+    contadorDistribuciones <- 1
+    while(contadorDistribuciones <= 7)  
+    {
       
-     
-    )    
+      ajuste = switch (contadorDistribuciones,
+                       fitdist(vectorDatos,"norm"),
+                       fitdist(vectorDatos,"unif"),
+                       fitdist(vectorDatos,"t", start=list(df=2)),
+                       #fitdist(vectorDatos,"chisq"),
+                       fitdist(vectorDatos/100,"exp"),
+                       fitdist(vectorDatos/100,"gamma"),
+                       fitdist(vectorDatos,"weibull"),
+                       #fitdist(vectorDatos,"binom", ),
+                       fitdist(vectorDatos,"pois",method="mle"),
+                       #fitdist(vectorDatos,"geom"),
+                       #fitdist(vectorDatos,"hyper"),
+                       #fitdist(vectorDatos,"nbinom")
+      )
+      
+      out <- tryCatch(
+        {
+          resultado <- gofstat(ajuste)
+          if(!is.null(resultado$kstest)){
+            if(grepl("not rejected", resultado$kstest)){
+              print(paste0("Dist: #",contadorDistribuciones))
+              print(resultado$kstest)
+              contadorDistribuciones = 13
+              numCorrectas = numCorrectas + 1
+            }
+          }
+          
+        }
+      )
+      
+      contadorDistribuciones = contadorDistribuciones + 1  
+        
+    }
+  
      i = i + 1
   }
+  
+  print(paste0("Correctas: ",numCorrectas))
   
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+#Grupo 1
+
+##Integrantes
+
+200418310 Jose Aguilar
+200715274 Sergio Mynor David Felipe Zapeta
+200915305 Sergio Alfonso Ferrer Garcia
